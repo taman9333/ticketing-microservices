b99c2fe1da0f8278a6769aded154bf58bdf575c3
taman9333 <taman9333@gmail.com>
2020-08-14 19:25:23 +0200

    Add jwt token with fake sign key

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -10,7 +11,15 @@ import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
+// as trafic is being proxied to our app through ingress
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
 import { User } from '../models/user';
@@ -31,6 +32,18 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const jwtToken = jwt.sign(
+      {
+        id: user.id,
+        email: user.email
+      },
+      'asdf'
+    );
+
+    // store it on session object
+    req.session = { jwt: jwtToken };
 
     res.status(201).send(user);
   }
