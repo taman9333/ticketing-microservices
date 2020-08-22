ee83cc19ce104dd6c0cdc1c6b62efe05cb29d0f5
taman9333 <taman9333@gmail.com>
2020-08-22 18:32:40 +0200

    Add extra test cases for signup

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,69 @@ it('returns 201 on successful signup', async () => {
     })
     .expect(201);
 });
+
+it('returns 400 with an invalid email', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'wrong',
+      password: 'password'
+    })
+    .expect(400);
+});
+
+it('returns 400 with an invalid password', async () => {
+  return request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: '22'
+    })
+    .expect(400);
+});
+
+it('returns 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com'
+    })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      password: 'password'
+    })
+    .expect(400);
+});
+
+it('Email must be unique', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(400);
+});
+
+it('Sets a cookie after successful signup', async () => {
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password'
+    })
+    .expect(201);
+
+  expect(response.get('Set-Cookie')).toBeDefined();
+});
