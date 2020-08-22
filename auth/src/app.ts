b287c7317e21182cfb4adb5c23d31b5e8969689c
taman9333 <taman9333@gmail.com>
2020-08-22 11:58:17 +0200

    Refactor some code in index file into app file in order to use supertest

---
+++ auth/src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler } from './middlewares/error-handler';
+
+const app = express();
+// as trafic is being proxied to our app through ingress
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
+
+app.use(errorHandler);
+
+export { app };
