9a7c353bd49d84adfab9497b3b941c132bad7a6b
taman9333 <taman9333@gmail.com>
2020-08-22 18:07:22 +0200

    Running first test successfully

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -2,15 +2,26 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
 
+// first download of MongoDB binaries may take a time.
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000;
+
 let mongo: any;
 beforeAll(async () => {
-  const mongo = new MongoMemoryServer();
+  process.env.JWT_KEY = 'test';
+
+  mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
 
-  await mongoose.connect(mongoUri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  });
+  await mongoose.connect(
+    mongoUri,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    },
+    (err) => {
+      if (err) console.error(err);
+    }
+  );
 });
 
 beforeEach(async () => {
@@ -22,6 +33,6 @@ beforeEach(async () => {
 });
 
 afterAll(async () => {
+  await mongoose.disconnect();
   await mongo.stop();
-  await mongoose.connection.close();
 });
