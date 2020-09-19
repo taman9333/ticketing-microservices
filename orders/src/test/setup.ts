e7ab9ca41a43b1cc81580bd915e05e57121efab7
taman9333 <taman9333@gmail.com>
2020-09-19 17:46:01 +0200

    Add tests for creating order

---
+++ orders/src/test/setup.ts
@@ -0,0 +1,66 @@
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
+
+// first download of MongoDB binaries may take a time.
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 600000;
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      getCookie(): string[];
+    }
+  }
+}
+
+jest.mock('../nats-wrapper');
+
+let mongo: any;
+beforeAll(async () => {
+  process.env.JWT_KEY = 'test';
+
+  mongo = new MongoMemoryServer();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(
+    mongoUri,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    },
+    (err) => {
+      if (err) console.error(err);
+    }
+  );
+});
+
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.disconnect();
+  await mongo.stop();
+});
+
+global.getCookie = () => {
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'test@test.com'
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+
+  const session = { jwt: token };
+
+  const sessionJson = JSON.stringify(session);
+
+  const base64 = Buffer.from(sessionJson).toString('base64');
+
+  return [`express:sess=${base64}`];
+};
