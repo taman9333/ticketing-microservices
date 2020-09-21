e7f62fb84fbd21c6216b86bb23805585384182be
taman9333 <taman9333@gmail.com>
2020-09-21 17:41:31 +0200

    Enqueue order expiration job

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,26 @@
+import { OrderCreatedEvent, Listener, Subjects } from '@ttickets/common';
+import { queueGroupName } from './queue-group-name';
+import { Message } from 'node-nats-streaming';
+import { expirationQueue } from '../../queues/expiration-queue';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+
+  readonly queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+    console.log('Waiting this many milleseconds to process the job', delay);
+
+    await expirationQueue.add(
+      {
+        orderId: data.id
+      },
+      {
+        delay
+      }
+    );
+
+    msg.ack();
+  }
+}
