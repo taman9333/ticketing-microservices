c3ea39ec359fcecc00b440fc797510d89cce265a
taman9333 <taman9333@gmail.com>
2020-09-22 07:36:01 +0200

    Payment creation

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,6 +3,7 @@ import request from 'supertest';
 import { OrderStatus } from '@ttickets/common';
 import { app } from '../../app';
 import { Order } from '../../models/order';
+import { Payment } from '../../models/payment';
 import { stripe } from '../../stripe';
 
 it('returns a 404 when purchasing an order that does not exist', async () => {
@@ -57,7 +58,7 @@ it('returns a 400 when purchasing a cancelled order', async () => {
     .expect(400);
 });
 
-it('returns a 204 with valid input', async () => {
+it('returns a 201 with valid input', async () => {
   const userId = mongoose.Types.ObjectId().toHexString();
   const order = Order.build({
     id: mongoose.Types.ObjectId().toHexString(),
@@ -78,8 +79,16 @@ it('returns a 204 with valid input', async () => {
     .expect(201);
 
   const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+  const chargeResult = await (stripe.charges.create as jest.Mock).mock
+    .results[0].value;
 
   expect(chargeOptions.source).toEqual('tok_visa');
   expect(chargeOptions.amount).toEqual(20 * 100);
   expect(chargeOptions.currency).toEqual('usd');
+
+  const payment = await Payment.findOne();
+
+  expect(payment).not.toBeNull();
+  expect(payment!.orderId).toEqual(order.id);
+  expect(payment!.stripeId).toEqual(chargeResult.id);
 });
